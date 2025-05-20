9cfc41f40ed0f1eb2fbebcf9f108234ca177a8ff
lhon901 <lhon901@163.com>
2025-05-20 22:28:09 +0800

    feat: exit command

--- app/main.py
+++ app/main.py
@@ -8,7 +8,26 @@ def main():
         sys.stdout.write("$ ")
         # Wait for user input
         command = input()
-        print(f"{command}: command not found")
+
+        import re
+
+        s = command + " "
+        space_idx = [x.start() for x in re.finditer(r" ", s)]
+        main_command = s[: space_idx[0]]
+        match main_command:
+            case "exit":
+                if len(space_idx) >= 2:
+                    parameter1 = s[space_idx[0] + 1 : space_idx[1]]
+                    try:
+                        n = int(parameter1)
+                    except:
+                        print(f"illegal parameter: {parameter1}")
+                    else:
+                        sys.exit(int(parameter1))
+                else:
+                    print(f"leak parameter")
+            case _:
+                print(f"{main_command}: command not found")
 
 
 if __name__ == "__main__":
