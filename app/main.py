6c3b57a0f8ca0fdf0901f0349becb8d84b514171
lhon901 <lhon901@163.com>
2025-05-23 23:25:02 +0800

    feat: redirect stdout

--- app/main.py
+++ app/main.py
@@ -2,6 +2,12 @@ import sys
 import os
 import subprocess
 import shlex
+import pty
+import io
+import struct
+import fcntl
+import termios
+import select
 
 
 def main():
@@ -13,8 +19,33 @@ def main():
         command = input().strip()
 
         s = shlex.split(command)
+        s[-1] = s[-1].strip()
+        # TODO: > strat should let stdout been read
         main_command = s[0]
-        args = s[1:]
+        redirect_idx = -1
+        try:
+            redirect_idx = s.index(">")
+        except:
+            pass
+
+        if redirect_idx == -1:
+            try:
+                redirect_idx = s.index("1>")
+            except:
+                pass
+
+        if redirect_idx == -1:
+            args = s[1:]
+        else:
+            args = s[1:redirect_idx]
+
+        original_stdout = sys.stdout
+        sys.stdout = slave_io
+        redirect = False
+        for i in s:
+            if i in redirect_symbols:
+                redirect = True
+                break
 
         match main_command:
             case "exit":
@@ -29,10 +60,10 @@ def main():
                     print(f"need parameter")
             case "echo":
                 if len(s) >= 2:
-                    s = ""
+                    v_args = ""
                     for i in args:
-                        s += i + " "
-                    print(s.strip())
+                        v_args += i + " "
+                    print(v_args.strip())
             case "type":
                 if len(s) >= 2:
                     if args[0] in builtin_command:
@@ -58,17 +89,48 @@ def main():
                 if main_command in path_command:
                     exec_path = f"{path_command[main_command]}"
                     if len(s) >= 2:
-                        subprocess.run([main_command, *args], executable=exec_path)
+                        subprocess.run(
+                            [main_command, *args],
+                            executable=exec_path,
+                            stdout=sys.stdout,
+                            stderr=sys.stderr,
+                        )
                     else:
-                        subprocess.run([main_command], executable=exec_path)
+                        subprocess.run(
+                            [main_command],
+                            executable=exec_path,
+                            stdout=sys.stdout,
+                            stderr=sys.stderr,
+                        )
                 else:
                     print(f"{main_command}: command not found")
+
+        sys.stdout = original_stdout
+        data = b""
+        rlist, _, _ = select.select([master], [], [], 0)
+        while True:
+            if rlist:
+                data += os.read(master, io.DEFAULT_BUFFER_SIZE)
+                if len(data) < io.DEFAULT_BUFFER_SIZE:
+                    break
+            else:
+                break
+
+        if redirect:
+            for idx, i in enumerate(s):
+                if i in redirect_symbols:
+                    filename = s[idx + 1 :]
+                    with open(filename[0], "wb") as f:
+                        f.write(data)
+        else:
+            print(f"{data.decode()}", end="")
 
 
 if __name__ == "__main__":
     PATH = os.getenv("PATH", None)
     HOME = os.getenv("HOME", None)
     builtin_command = ["exit", "echo", "type", "pwd", "cd"]
+    redirect_symbols = [">", "1>"]
     # env path
     path_command = {}
     if PATH:
@@ -80,4 +142,19 @@ if __name__ == "__main__":
                         if file in path_command:
                             continue
                         path_command[file] = file_path
+    # pty
+    master, slave = pty.openpty()
+    size = os.get_terminal_size()
+    rows = size.lines
+    cols = size.columns
+    winsize = struct.pack("HHHH", rows, cols, 0, 0)
+    fcntl.ioctl(slave, termios.TIOCSWINSZ, winsize)
+    slave_io = os.fdopen(slave, "w")
+    # platform
+    is_windows = os.name == "nt"
+    if not is_windows:
+        attrs = termios.tcgetattr(slave)
+        attrs[1] &= ~termios.ONLCR
+        termios.tcsetattr(slave, termios.TCSANOW, attrs)
+
     main()
